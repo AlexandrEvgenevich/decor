6167406eccbda212dd8b93b78e10acde68993bd5
AleksandrEgorov <ferociousrat@mail.ru>
2022-06-22 12:26:49 +0300

    dz decor

--- decor.py
+++ decor.py
@@ -42,7 +42,32 @@ def decor1(fun, log_path):
     return new_fun
 
 
+def decor2(log_path):
+    def dec(fun):
+        def new_fun(*args, **kwargs):
+            lp = log_path
+            res = dec(*args, **kwargs)
+            a = f"function {fun.__name__} "
+            b = f"at {str(datetime.datetime.now())} "
+            c = f"with args {str(args)}, and kwargs {str(kwargs)}\n"
+
+            with open(lp, 'a', encoding='utf=8') as f:
+                f.write(a)
+                f.write(b)
+                f.write(c)
+
+            return res
+        return new_fun
+    return dec
+
+
 rat = decor1(rat_fun, 'log.txt')
+
+
+@decor2('log.txt')
+def rat2():
+    rat_fun()
+
 
 rat()
 
